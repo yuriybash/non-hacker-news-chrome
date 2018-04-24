bcf3af41371821e55a8b3e0a010233d59adea4b2
Yuriy Bash <yuriybash@gmail.com>
2018-04-24 15:31:33 -0400

    return early in background.js

--- background.js
+++ background.js
@@ -22,3 +22,29 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   }
   return true;
 });
+
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+
+  if(!(msg.from === 'content')){
+    return undefined;
+  }
+
+  if (msg.subject === 'showPageAction') {
+    chrome.pageAction.show(sender.tab.id);
+  } else if(msg.subject === 'filter_request'){
+
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("POST", predictor_endpoint);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    xmlhttp.onreadystatechange = function() { // Call a function when the state changes.
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        let predictions = JSON.parse(this.responseText)['prediction'];
+        sendResponse(predictions);
+      }
+    };
+    xmlhttp.send(JSON.stringify({"data": msg.data}));
+
+  }
+  return true;
+});
