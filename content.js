faed20874ae8c6ac3c4ac8c3322959b0e34024d7
Yuriy Bash <yuriybash@gmail.com>
2018-05-26 08:31:08 -0400

    allow for filtering from more strict to less strict

--- content.js
+++ content.js
@@ -3,23 +3,20 @@ chrome.runtime.sendMessage({
     subject: 'showPageAction'
 });
 
-function get_threshold(filter_level) {
-    // Obtained via previous regression
-    return 0.03354 * filter_level - 0.06227
-}
+
+const get_threshold = function (f_level) {return 0.03354 * f_level - 0.06227};
 
 function filter_hn_stories(filter_level) {
 
-    let story_links = Array.prototype.slice.call(document.getElementsByClassName('storylink'));
-    let titles_urls = story_links.map(function(story_link) {
+    let story_links = Array.prototype.slice.call(
+        document.getElementsByClassName('storylink'));
 
+    let titles_urls = story_links.map(function(story_link) {
         let title = story_link.text.replace(/[^a-zA-Z0-9 -]/, '').toLowerCase();
         let domain_parts = new URL(story_link.href).hostname.split(".");
-        if (domain_parts.length === 2) {
-            return [title, domain_parts[0]]
-        } else {
-            return [title, domain_parts[1]]
-        }
+        let domain = (domain_parts.length === 2) ? domain_parts[0] : domain_parts[1];
+        console.log("domain: " + domain);
+        return [title, domain]
     });
 
     chrome.runtime.sendMessage({
@@ -34,6 +31,8 @@ function filter_hn_stories(filter_level) {
         for (let i = 0; i < things.length; i++) {
             if (response[i][1] <= threshold) {
                 things[i].style.display = 'none'
+            } else {
+                things[i].style.display = ''
             }
         }
     });
@@ -49,7 +48,10 @@ chrome.runtime.onMessage.addListener(function(msg) {
 
     if (msg.subject === 'filterHN') {
         filter_hn_stories(parseInt(msg.filter_level));
-    } else if (msg.subject === 'reset') {
+        return
+    }
+
+    if (msg.subject === 'reset') {
         let things = document.getElementsByClassName('athing');
         for (let i = 0; i < things.length; i++) {
             things[i].style.display = ''
