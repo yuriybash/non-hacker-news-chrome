be1bae9f59c5d1896cea6031f622e90b83fbe4ee
Yuriy Bash <yuriybash@gmail.com>
2018-11-20 05:01:13 -0400

    update content.js

--- content.js
+++ content.js
@@ -21,12 +21,10 @@ function filter_hn_stories(filter_level){
     });
 
     chrome.runtime.sendMessage({from: "content", subject: "filter_request", data: titles_urls}, function(response) {
-        console.log("in content.js, response: ");
-        console.log(response);
 
         let threshold = 0.03354*filter_level-0.06227
-
         let things = document.getElementsByClassName('athing')
+
         for(var i = 0; i < things.length; i++){
             if(response[i][1] <= threshold){
                 things[i].style.display= 'none'
@@ -37,14 +35,15 @@ function filter_hn_stories(filter_level){
 }
 
 
-// Listen for messages from the popup
 chrome.runtime.onMessage.addListener(function (msg, sender, response) {
-    // First, validate the message's structure
-    if ((msg.from === 'popup') && (msg.subject === 'filterHN')) {
-        filter_hn_stories(parseInt(msg.filter_level));
-
-        // Directly respond to the sender (popup),
-        // through the specified callback */
-        // response("response from content.js");
+    if (msg.from === 'popup') {
+        if(msg.subject === 'filterHN'){
+            filter_hn_stories(parseInt(msg.filter_level));
+        } else if (msg.subject === 'reset'){
+            let things = document.getElementsByClassName('athing')
+            for(var i = 0; i < things.length; i++){
+                things[i].style.display= ''
+            }
+        }
     }
 });
