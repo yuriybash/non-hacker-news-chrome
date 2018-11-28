820cd67c327d43776f24221cef3aa4b61b2edfef
Yuriy Bash <yuriybash@gmail.com>
2018-11-28 06:31:33 -0400

    update popup.js

--- popup.js
+++ popup.js
@@ -5,11 +5,14 @@
 'use strict';
 
 let changeColor = document.getElementById('changeColor');
+let slider = document.getElementById('hn_tech_range');
+let slider_value = document.getElementById('slider_output')
 
-chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-});
+slider.oninput = function(){
+    console.log("updating value");
+    slider_value.innerHTML = this.value;
+}
+
 
 
 function filterComplete(info) {
@@ -21,10 +24,13 @@ changeColor.onclick = function(element){
         active: true,
         currentWindow: true
     }, function (tabs) {
+
+        let filter_level = document.getElementById('hn_tech_range').value;
+
         // ...and send a request for the DOM info...
         chrome.tabs.sendMessage(
             tabs[0].id,
-            {from: 'popup', subject: 'filterHN'},
+            {from: 'popup', subject: 'filterHN', filter_level: filter_level},
             // ...also specifying a callback to be called
             //    from the receiving end (content script)
             filterComplete);
