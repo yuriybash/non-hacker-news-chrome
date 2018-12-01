31bbc66904f357fa5b47d6d6ce1489c9406d014d
Yuriy Bash <yuriybash@gmail.com>
2018-12-01 15:21:11 -0400

    beautify popup.js

--- popup.js
+++ popup.js
@@ -5,38 +5,43 @@ let reset_button = document.getElementById('reset');
 let slider = document.getElementById('hn_tech_range');
 let slider_value = document.getElementById('slider_output')
 
-slider.oninput = function(){
+slider.oninput = function() {
     slider_value.innerHTML = this.value;
 }
 
-filter_button.onclick = function(element){
+filter_button.onclick = function(element) {
     chrome.tabs.query({
         active: true,
         currentWindow: true
-    }, function (tabs) {
+    }, function(tabs) {
 
         let filter_level = document.getElementById('hn_tech_range').value;
 
         // ...and send a request for the DOM info...
         chrome.tabs.sendMessage(
-            tabs[0].id,
-            {from: 'popup', subject: 'filterHN', filter_level: filter_level},
+            tabs[0].id, {
+                from: 'popup',
+                subject: 'filterHN',
+                filter_level: filter_level
+            },
             // ...also specifying a callback to be called
             //    from the receiving end (content script)
-            );
+        );
     });
 };
 
-reset_button.onclick = function(element){
+reset_button.onclick = function(element) {
     chrome.tabs.query({
         active: true,
         currentWindow: true
-    }, function (tabs) {
+    }, function(tabs) {
 
 
         chrome.tabs.sendMessage(
-            tabs[0].id,
-            {from: 'popup', subject: 'reset'},
-            );
+            tabs[0].id, {
+                from: 'popup',
+                subject: 'reset'
+            },
+        );
     });
 };
