d9ad3c961a547746545378b8a8057b60c9de73f7
Yuriy Bash <yuriybash@gmail.com>
2018-12-24 05:41:44 -0400

    update popup.js

--- popup.js
+++ popup.js
@@ -1,25 +1,15 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
-let changeColor = document.getElementById('changeColor');
+let filter_button = document.getElementById('filter');
+let reset_button = document.getElementById('reset');
 let slider = document.getElementById('hn_tech_range');
 let slider_value = document.getElementById('slider_output')
 
 slider.oninput = function(){
-    console.log("updating value");
     slider_value.innerHTML = this.value;
 }
 
-
-
-function filterComplete(info) {
-    console.log("in filterComplete in popup.js")
-}
-
-changeColor.onclick = function(element){
+filter_button.onclick = function(element){
     chrome.tabs.query({
         active: true,
         currentWindow: true
@@ -33,6 +23,20 @@ changeColor.onclick = function(element){
             {from: 'popup', subject: 'filterHN', filter_level: filter_level},
             // ...also specifying a callback to be called
             //    from the receiving end (content script)
-            filterComplete);
+            );
+    });
+};
+
+reset_button.onclick = function(element){
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function (tabs) {
+
+
+        chrome.tabs.sendMessage(
+            tabs[0].id,
+            {from: 'popup', subject: 'reset'},
+            );
     });
 };
