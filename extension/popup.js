98614e170ae8e25f0fccdb58c2879a5718c4ba77
Yuriy Bash <yuriybash@gmail.com>
2019-01-29 18:33:45 -0500

    minor cleanup in popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -3,11 +3,11 @@
 let filter_button = document.getElementById('filter');
 let reset_button = document.getElementById('reset');
 let slider = document.getElementById('hn_tech_range');
-let slider_value = document.getElementById('slider_output')
+let slider_value = document.getElementById('slider_output');
 
 slider.oninput = function() {
     slider_value.innerHTML = this.value;
-}
+};
 
 filter_button.onclick = function(element) {
     chrome.tabs.query({
@@ -15,17 +15,14 @@ filter_button.onclick = function(element) {
         currentWindow: true
     }, function(tabs) {
 
-        let filter_level = document.getElementById('hn_tech_range').value;
+        const filter_level = document.getElementById('hn_tech_range').value;
 
-        // ...and send a request for the DOM info...
         chrome.tabs.sendMessage(
             tabs[0].id, {
                 from: 'popup',
                 subject: 'filterHN',
                 filter_level: filter_level
             },
-            // ...also specifying a callback to be called
-            //    from the receiving end (content script)
         );
     });
 };
@@ -35,7 +32,6 @@ reset_button.onclick = function(element) {
         active: true,
         currentWindow: true
     }, function(tabs) {
-
 
         chrome.tabs.sendMessage(
             tabs[0].id, {
